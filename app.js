b74c5b1c994504f3d0f6bb7d085384e65711f7dd
ayelabilal <ayelabilal944@gmail.com>
2024-03-14 01:06:16 +0500

    continue

---
+++ app.js
@@ -0,0 +1,27 @@
+// ARRAYS AND LOOP
+// q:3
+var counting = ""
+
+// q:4
+var userInput = +prompt("Enter a number to generate a table")
+
+var tableRange = +prompt("enter table range ")
+
+for (var i = 1 ; i <= tableRange; i++){
+    document.write(userInput + "x" + i + "=" + userInput * i + "<br>" )
+
+}
+// q:5
+var fruits = ["apple","banana","mango","orange","strawberry"]
+document.write(fruits)
+
+// q:6
+
+
+// q:1
+
+// q:1
+
+
+
+
